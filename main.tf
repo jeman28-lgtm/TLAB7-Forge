8ce82eee220b7c0f80719aabe73ef0d586bbf2c5
James Emanuel <jeman28@gmail.com>
2026-08-02 13:06:17 -0400

    Secured ingress rules

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_security_group" "sabotaged_sg" {
     from_port   = 22  
     to_port     = 22  
     protocol    = "tcp"  
-    cidr_blocks = ["0.0.0.0/0"] # SABOTAGE: SSH exposed to the world  
+    cidr_blocks = ["108.27.221.246/32"] # SABOTAGE: SSH exposed to the world  
   }  
 }  
